2983ac7842f21ddd931884513399a2694c6c0ad5
CoddeX Developer <gabrielrodrigueslima22@gmail.com>
2026-03-03 02:35:35 -0300

    ajustando a responsividade do component modal

--- app/components/FormComponent/index.tsx
+++ app/components/FormComponent/index.tsx
@@ -19,73 +19,46 @@ import { useIsOpen } from "@/app/contexts/isOpenContext"
 export function FormComponent() {
     const { setIsOpen } = useIsOpen()
     return (
-        <div className="relative flex items-center justify-center min-h-[100dvh] overflow-hidden">
+        <>
+            <Card className="w-sm md:w-lg lg:max-w-xl backdrop-blur-4xl p-0">
+                <CardHeader className="border-b dark:bg-gray-900/60 border-border p-6 text-center">
+                    <CardTitle className="text-2xl dark:dark:text-neutral-200">
+                        Entre em contato
+                    </CardTitle>
 
-            {/* Background Blur Glow */}
-            <div
-                className="
-          absolute
-        "
-            />
-
-            {/* FORM */}
-            <Card className="relative w-full  border-none bg-transparent shadow-none">
-                <MagicCard
-                    className="
-            p-0
-            backdrop-blur-xl
-            bg-white/70
-            dark:bg-neutral-900/70
-            shadow-[0_20px_60px_rgba(0,0,0,0.25)]
-            rounded-2xl
-          "
-                >
-                    <CardHeader className="border-b border-border p-6 text-center">
-                        <CardTitle className="text-2xl">
-                            Entre em contato
-                        </CardTitle>
-
-                        <CardDescription>
-                            Envie uma mensagem e retornarei em breve
-                        </CardDescription>
-                    </CardHeader>
-
-                    <CardContent className="p-6 w-sm lg:w-xl">
-                        <form className="grid gap-5">
-
-                            {/* Nome */}
-                            <div className="grid gap-2">
-                                <Label htmlFor="name">Nome</Label>
-                                <Input
-                                    id="name"
-                                    placeholder="Seu nome"
-                                />
-                            </div>
-
-                            {/* Mensagem */}
-                            <div className="grid gap-2">
-                                <Label htmlFor="message">Mensagem</Label>
-                                <Textarea
-                                    id="message"
-                                    placeholder="Digite sua mensagem..."
-                                    rows={5}
-                                    className="resize-none h-30 lg:h-40"
-                                />
-                            </div> 
-
-                        </form>
-                    </CardContent>
-
-                    <CardFooter className="border-t border-border grid gap-10 grid-cols-2 p-6">
-                        <Button onClick={() => setIsOpen(false)} variant={"outline"} type="button" className="cursor-pointer w-full text-neutral-600 dark:text-neutral-200">
-                            Cancelar
-                        </Button>
-                        <Button type="button" className="cursor-pointer w-full bg-green-400 hover:bg-green-500 dark:text-neutral-100">
-                            Enviar mensagem
-                        </Button>
-                    </CardFooter>
-                </MagicCard>
+                    <CardDescription>
+                        Envie uma mensagem e retornarei em breve
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form className="grid gap-5">
+                        <div className="grid gap-2">
+                            <Label className="dark:text-neutral-200" htmlFor="name">Nome</Label>
+                            <Input
+                                id="name"
+                                placeholder="Seu nome"
+                                className="dark:text-neutral-200" />
+                        </div>
+                        <div className="grid gap-2">
+                            <Label className="dark:text-neutral-200" htmlFor="message">Mensagem</Label>
+                            <Textarea
+                                id="message"
+                                placeholder="Digite sua mensagem..."
+                                rows={5}
+                                className="resize-none h-30 lg:h-40 dark:text-neutral-200"
+                            />
+                        </div>
+                    </form>
+                </CardContent>
+                <CardFooter className="border-t dark:bg-gray-900/60 border-border grid gap-7 md:gap-10 grid-cols-2 p-6">
+                    <Button onClick={() => setIsOpen(false)} variant={"outline"} type="button" className=" hover:-translate-y-0.5 hover:text-neutral-600 transition duration-200 cursor-pointer w-full text-neutral-600 dark:dark:text-neutral-200">
+                        Cancelar
+                    </Button>
+                    <Button type="button" className="hover:-translate-y-0.5 transition duration-200 cursor-pointer w-full bg-green-600 dark:bg-slate-600 hover:bg-green-500 dark:hover:bg-slate-500 dark:text-neutral-100">
+                        Enviar mensagem
+                    </Button>
+                </CardFooter>
             </Card>
-        </div >
+        </>
     )
 }

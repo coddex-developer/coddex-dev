4f5eb17958e0b6aa1fa0b098f4e0832932614ee4
CoddeX Developer <gabrielrodrigueslima22@gmail.com>
2026-04-30 00:46:09 -0300

    ajuste de posicionamento do layout

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default function Page() {
     return (
         <>
-            <Card className="relative min-h-screen w-full p-0 gap-0">
-                <CardHeader className="py-4 border-b">
+            <Card className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-screen bg-transparent w-full gap-0 rounded-0">
+                <CardHeader className="py-4 border-b rounded-0">
                     <h1>Header</h1>
                 </CardHeader>
-                <CardContent className="grid grid-cols-6 p-0">
-                    <div className="min-h-screen col-span-1 px-3 flex flex-col gap-8 justify-between py-8">
+                <CardContent className="flex-1 grid grid-cols-6 p-0">
+                    <div className="h-full hidden md:flex md:col-span-1 sticky flex-col gap-8 justify-between py-8">
                         <ul className="list-none">
                             <li>Home</li>
                             <li>Projetos</li>
@@ -17,10 +17,10 @@ export default function Page() {
                         </ul>
                         <div className="flex flex-col gap-2">
                             <hr className="bg-gray-100" />
-                            <Link className="bg-linear-to-br from-gray-700/50 to-blue-500/50 bg- rounded-2xl text-center py-1 block" href={"/"}>Sair</Link>
+                            <Link className="bg-linear-to-br w-11/12 mx-auto from-gray-700/50 to-blue-500/50 bg- rounded-2xl text-center py-1 block" href={"/"}>Sair</Link>
                         </div>
                     </div>
-                    <div className="bg-yellow-500 col-span-5">
+                    <div className="w-screen mx-auto bg-gray-800/50 col-span-6 md:col-span-5">
                         <h1>
                             Contents
                         </h1>

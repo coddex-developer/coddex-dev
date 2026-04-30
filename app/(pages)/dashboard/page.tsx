7f16dddf1d9c74e1398cfb2604def2a990c1770e
CoddeX Developer <gabrielrodrigueslima22@gmail.com>
2026-04-29 23:25:08 -0300

    refactor: ajustar estilos do cabeçalho e rodapé no layout do dashboard

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -5,11 +5,11 @@ export default function Page() {
     return (
         <>
             <Card className="relative min-h-screen w-full p-0 gap-0">
-                <CardHeader className="bg-purple-500 py-4">
+                <CardHeader className="py-4 border-b">
                     <h1>Header</h1>
                 </CardHeader>
                 <CardContent className="grid grid-cols-6 p-0">
-                    <div className="bg-orange-900 min-h-full col-span-1 px-3 flex flex-col gap-8 justify-between py-8">
+                    <div className="min-h-screen col-span-1 px-3 flex flex-col gap-8 justify-between py-8">
                         <ul className="list-none">
                             <li>Home</li>
                             <li>Projetos</li>
@@ -17,7 +17,7 @@ export default function Page() {
                         </ul>
                         <div className="flex flex-col gap-2">
                             <hr className="bg-gray-100" />
-                            <Link className="bg-red-700/60 rounded-2xl text-center py-1 block" href={"/"}>Sair</Link>
+                            <Link className="bg-linear-to-br from-gray-700/50 to-blue-500/50 bg- rounded-2xl text-center py-1 block" href={"/"}>Sair</Link>
                         </div>
                     </div>
                     <div className="bg-yellow-500 col-span-5">
@@ -26,7 +26,7 @@ export default function Page() {
                         </h1>
                     </div>
                 </CardContent>
-                <CardFooter className="bg-blue-500 py-4">
+                <CardFooter className="py-4 border-t">
                     <h1>Footer</h1>
                 </CardFooter>
             </Card>

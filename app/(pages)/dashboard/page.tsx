318ad7d92ad33f61dc03bac27a4162906d26582e
CoddeX Developer <gabrielrodrigueslima22@gmail.com>
2026-04-29 23:12:43 -0300

    refactor: substituir a estrutura da página do dashboard para um layout de cartão com cabeçalho, conteúdo e rodapé

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,7 +1,35 @@
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import Link from "next/link";
+
 export default function Page() {
     return (
         <>
-            <h1>Olá admin</h1>
+            <Card className="relative min-h-screen w-full p-0 gap-0">
+                <CardHeader className="bg-purple-500 py-4">
+                    <h1>Header</h1>
+                </CardHeader>
+                <CardContent className="grid grid-cols-6 p-0">
+                    <div className="bg-orange-900 min-h-full col-span-1 px-3 flex flex-col gap-8 justify-between py-8">
+                        <ul className="list-none">
+                            <li>Home</li>
+                            <li>Projetos</li>
+                            <li>Configurações</li>
+                        </ul>
+                        <div className="flex flex-col gap-2">
+                            <hr className="bg-gray-100" />
+                            <Link className="bg-red-700/60 rounded-2xl text-center py-1 block" href={"/"}>Sair</Link>
+                        </div>
+                    </div>
+                    <div className="bg-yellow-500 col-span-5">
+                        <h1>
+                            Contents
+                        </h1>
+                    </div>
+                </CardContent>
+                <CardFooter className="bg-blue-500 py-4">
+                    <h1>Footer</h1>
+                </CardFooter>
+            </Card>
         </>
     )
 }
